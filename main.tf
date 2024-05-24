6b87117fab6ab2a36510f5648019f1fcf857949a
anil <edigaanilkumar2000@gmail.com>
2024-05-24 16:20:26 +0530

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
+provider "aws" {
+ region = "ap-south-1"
+}
+
+
 resource "aws_instance" "local" {
   ami = "ami-0cc9838aa7ab1dce7"
-  instance_type="t2.micro"
-  key_name=my-keypair-01
+  instance_type="t2.nano"
+  tags = {
+    Name="raja"
+  }
   }
